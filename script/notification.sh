7f72c99548e03f8f09595c25cfe82952eb97ff3a
xyzuan <xyzuannihboss@gmail.com>
2023-11-03 01:11:40 +0700

    fs

--- script/notification.sh
+++ script/notification.sh
@@ -1,12 +1,12 @@
 echo "HEALTH_STATUS: $HEALTH_STATUS"
-echo "GITHUB_TOKEN: $GITHUB_TOKEN"
+echo "GITHUB_TOKEN: $GH_TOKEN"
  
 if [[ $HEALTH_STATUS == *"failed"* ]]; then
   curl \
     -X POST \
-    -H "authorization: Bearer $GITHUB_TOKEN" \
+    -H "authorization: Bearer $GH_TOKEN" \
     -H "Accept: application/vnd.github.v3+json" \
-    https://api.github.com/repos/zernonia/statusbase/issues \
+    https://api.github.com/repos/xyzuan/status/issues \
     -d '{"title":"Website is down","body":"'$HEALTH_STATUS'","labels":["failed"]}'
 fi
 
